c561429f035ceda14e2344391a1cb4a73b39209e
dlrbdjs <gon2yq@naver.com>
2025-02-19 19:51:44 +0900

    Add dataSource Bean

--- Lee/src/main/java/SpringConfig.java
+++ Lee/src/main/java/SpringConfig.java
@@ -1,6 +1,7 @@
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import repository.JdbcTemplateMemberRepository;
 import repository.MemberRepository;
 
@@ -8,15 +9,26 @@ import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
-    private final DataSource dataSource;
-
-    @Autowired
-    public SpringConfig(DataSource dataSource) {
-        this.dataSource = dataSource;
-    }
 
     @Bean
-    MemberRepository memberRepository() {
-        return new JdbcTemplateMemberRepository(dataSource);
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/dbpractice");
+        dataSource.setUsername("root");
+        dataSource.setPassword("1234");
+        return dataSource;
+    }
+
+//    private final DataSource dataSource;
+//
+//    @Autowired
+//    public SpringConfig(DataSource dataSource) {
+//        this.dataSource = dataSource;
+//    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+        return new JdbcTemplateMemberRepository(dataSource());
     }
 }
