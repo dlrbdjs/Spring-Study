f31f3032f09d7c342dc5b4f97fb051a4bd69fc6a
dlrbdjs <gon2yq@naver.com>
2025-03-04 00:17:44 +0900

    Add save method

--- demo/src/main/java/practice/demo/member/controller/MemberController.java
+++ demo/src/main/java/practice/demo/member/controller/MemberController.java
@@ -2,6 +2,8 @@ package practice.demo.member.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MemberController {
@@ -11,4 +13,12 @@ public class MemberController {
         return "save";
     }
 
+    @PostMapping("/member/save")
+    public String save(@RequestParam("memberEmail") String memberEmail,
+                       @RequestParam("memberPassword") String memberPassword,
+                       @RequestParam("memberName") String memberName) {
+        System.out.println("MemberController.save");
+        System.out.println("memberEmail = " + memberEmail);
+        return "index";
+    }
 }
