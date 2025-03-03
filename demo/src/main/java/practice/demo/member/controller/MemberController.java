b5d6dcbb3f7c886846969ba9a8c6b1b172dddafc
dlrbdjs <gon2yq@naver.com>
2025-03-04 00:49:31 +0900

    Refactor save method

--- demo/src/main/java/practice/demo/member/controller/MemberController.java
+++ demo/src/main/java/practice/demo/member/controller/MemberController.java
@@ -1,24 +1,40 @@
 package practice.demo.member.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import practice.demo.member.dto.MemberDTO;
+import practice.demo.member.service.MemberService;
 
 @Controller
+@RequiredArgsConstructor
 public class MemberController {
+    // 생성자 주입
+    private final MemberService memberService;
+
     // 회원가입 페이지 출력 요청
     @GetMapping("/member/save")
     public String saveForm() {
         return "save";
     }
 
+//    @PostMapping("/member/save")
+//    public String save(@RequestParam("memberEmail") String memberEmail,
+//                       @RequestParam("memberPassword") String memberPassword,
+//                       @RequestParam("memberName") String memberName) {
+//        System.out.println("MemberController.save");
+//        System.out.println("memberEmail = " + memberEmail);
+//        return "index";
+//    }
+
     @PostMapping("/member/save")
-    public String save(@RequestParam("memberEmail") String memberEmail,
-                       @RequestParam("memberPassword") String memberPassword,
-                       @RequestParam("memberName") String memberName) {
+    public String save(@ModelAttribute MemberDTO memberDTO) {
         System.out.println("MemberController.save");
-        System.out.println("memberEmail = " + memberEmail);
+        System.out.println("memberDTO = " + memberDTO);
+        memberService.save(memberDTO);
         return "index";
     }
 }
