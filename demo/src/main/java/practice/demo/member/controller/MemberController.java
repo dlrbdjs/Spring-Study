8c1ba6fd71292804ba3830ca538aa2cca39d7fda
dlrbdjs <gon2yq@naver.com>
2025-03-05 23:10:14 +0900

    Add checking email redundancy function

--- demo/src/main/java/practice/demo/member/controller/MemberController.java
+++ demo/src/main/java/practice/demo/member/controller/MemberController.java
@@ -97,4 +97,17 @@ public class MemberController {
         session.invalidate();
         return "success";
     }
+
+    @PostMapping("/member/email-check")
+    public @ResponseBody String emailCheck(@RequestParam("memberEmail") String memberEmail) {
+        System.out.println("memberEmail = " + memberEmail);
+        String checkResult = memberService.emailCheck(memberEmail);
+        return checkResult;
+//        if (checkResult != null) {
+//            return "ok";
+//        } else {
+//            return "no";
+//        }
+    }
+
 }
